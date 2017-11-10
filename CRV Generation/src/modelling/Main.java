7835a74d50808595b1b37d6099cc9aecd44073c2
EXADEL\avertsinskaya <avertsinskaya@exadel.com>
2017-11-10 15:16:02 +0300

    Count correlation coefficient for BoxMullerGenerator's for samples with odd and even indexes

--- CRV Generation/src/modelling/Main.java
+++ CRV Generation/src/modelling/Main.java
@@ -1,5 +1,8 @@
 package modelling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import modelling.continuous.BoxMullerGenerator;
 import modelling.continuous.FisherGenerator;
 import modelling.continuous.LogisticGenerator;
@@ -15,6 +18,7 @@ public class Main {
 		int basicValuesNumber = 192; // 192, 48
 		NormalGenerator nGenerator = new NormalGenerator(mean, dispersion, basicValuesNumber);
 		nGenerator.generate(n);
+		System.out.println("Normal distribution");
 		//System.out.println(nGenerator.getGeneratedSequence());
 		System.out.println(nGenerator.getMathExpectationComparison());
 		System.out.println(nGenerator.getDispersionComparison() + '\n');
@@ -22,6 +26,7 @@ public class Main {
 		int a = 2, b = 3;
 		LogisticGenerator lGenerator = new LogisticGenerator(a, b);
 		lGenerator.generate(n);
+		System.out.println("Logistic distribution");
 	   // System.out.println(lGenerator.getGeneratedSequence());
 	    System.out.println(lGenerator.getMathExpectationComparison());
 		System.out.println(lGenerator.getDispersionComparison() + '\n');
@@ -29,6 +34,7 @@ public class Main {
 		int l = 5, m = 3;
 		FisherGenerator fGenerator = new FisherGenerator(l, m);
 		fGenerator.generate(n);
+		System.out.println("Fisher distribution");
 		//System.out.println(fGenerator.getGeneratedSequence());
 		System.out.println(fGenerator.getMathExpectationComparison());
 		System.out.println(fGenerator.getDispersionComparison() + '\n');
@@ -36,15 +42,32 @@ public class Main {
 		double p = 0.6;
 		MixtureDistributionGenerator mGenerator = new MixtureDistributionGenerator(lGenerator, nGenerator, 0.6);
 		mGenerator.generate(n);
+		System.out.println("Mixture distribution");
 	   // System.out.println(mGenerator.getGeneratedSequence());
 		System.out.println(mGenerator.getMathExpectationComparison());
 		System.out.println(mGenerator.getDispersionComparison() + '\n');
 		
 		BoxMullerGenerator bGenerator = new BoxMullerGenerator();
 		bGenerator.generate(n);
+		System.out.println("Box-Muller distribution");
 		//System.out.println(bGenerator.getGeneratedSequence());
 		System.out.println(bGenerator.getMathExpectationComparison());
 		System.out.println(bGenerator.getDispersionComparison() + '\n');
+		
+		
+		List<Double> s1 = new ArrayList<>();
+		List<Double> s2 = new ArrayList<>();
+		
+		List<Double> seq = bGenerator.getGeneratedSequence();
+		for (int i = 0; i < seq.size(); i ++) {
+			if (i % 2 == 0) {
+				s1.add(seq.get(i));
+			} else {
+				s2.add(seq.get(i));
+			}
+		}
+		
+		System.out.println("Correlation Coefficient: " + bGenerator.getCorrelationCoefficient(s1, s2));
 	}
 
 }
--- CRV Generation/src/modelling/continuous/BoxMullerGenerator.java
+++ CRV Generation/src/modelling/continuous/BoxMullerGenerator.java
@@ -40,4 +40,21 @@ public class BoxMullerGenerator extends Generator {
 	public double getRealDispersion() {
 		return 1;
 	}
+	
+	public double getCorrelationCoefficient(List<Double> s1, List<Double> s2) {
+		int n = s1.size();
+		double a = 0;
+		
+		double s1Mean = s1.stream().mapToDouble(e -> e).sum() / n;
+		double s2Mean = s2.stream().mapToDouble(e -> e).sum() / n;
+		
+		for (int i = 0; i < n; i ++) {
+			a += (s1.get(i) - s1Mean) * (s2.get(i) - s2Mean);
+		}
+		
+		double sum1 = s1.stream().mapToDouble(e -> Math.pow(e - s1Mean, 2)).sum();
+		double sum2 = s2.stream().mapToDouble(e -> Math.pow(e - s2Mean, 2)).sum();
+		
+		return a / Math.sqrt(sum1 * sum2);
+	}
 }
